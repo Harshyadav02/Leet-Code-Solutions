71bafc18403f579ef623dc5cc04bc2b11ee4cbc6
harshyadav02 <hy043768@gmail.com>
2023-08-16 09:42:22 +0530

    Leet code problem 1 two sum

---
+++ Easy/01_Two_Sum/1_Two_sum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+         HashMap<Integer, Integer> hashMap = new HashMap<>(); // Create a hash map to store value-index pairs
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (hashMap.containsKey(complement)) {
+                return new int[]{hashMap.get(complement), i}; // Return the indices if the complement is found
+            }
+            
+            hashMap.put(nums[i], i); // Store the current value with its index in the hash map
+        }
+        
+        return null; // Return null if no such pair is found
+    }
+}
