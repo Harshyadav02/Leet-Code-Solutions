c76b54ec71a218223e2598e2cbe46a5c1bf5cca0
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2023-08-29 14:26:46 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        if(head==null||head.next==null) return head;
+        Node temp=head;
+        Stack<Node> st=new Stack<>();
+        st.push(head);
+        temp=temp.next;
+        while(temp!=null){
+            
+            while(!st.isEmpty()&&temp.data>st.peek().data)
+                  st.pop();
+            st.push(temp);
+            temp=temp.next;
+        }
+        head=null;
+        while(!st.isEmpty()){
+            temp=st.pop();
+            temp.next=head;
+            head=temp;
+        }
+        return head;
+    }
+}
+  
