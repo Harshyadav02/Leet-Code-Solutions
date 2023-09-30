d757356ab999940643cf944fbcf566759190bdf5
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2023-09-30 10:24:19 +0530

    Time: 15 ms (27.35%), Space: 43.7 MB (34.64%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        HashMap <Character , Integer> map = new HashMap<>();
+        
+        
+        // inserting all the s elements to map with number of frequency
+        for(int i = 0; i<s.length(); i++){
+            
+            char ch = s.charAt(i);
+            
+            map.put(ch , map.getOrDefault(ch, 0) + 1);
+            
+        }
+        
+        
+        // iteration for t string 
+        for(int i= 0; i <t.length(); i++){
+            
+            char ch = t.charAt(i);
+            
+            if(map.get(ch)!=null){
+                
+                
+                // if the value for the character  of t is 1 then remove it 
+                if(map.get(ch) == 1){
+                    map.remove(ch);
+                } 
+                else{
+                    
+                    // if the value for the key is  greater then 1 then decrese it by 1
+                    map.put(ch , map.get(ch) -1);
+                }
+            } 
+            else{
+                return false ;
+            }
+        }
+        
+        
+        return map.isEmpty();
+    }
+}
