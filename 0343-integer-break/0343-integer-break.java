39f9f813098e26b0ff0bd678790211ddacf4c837
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2023-10-06 21:43:07 +0530

    Time: 0 ms (100.00%), Space: 39 MB (63.93%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -11,15 +11,22 @@ class Solution {
         if(n == 3){
             return 2 ;
         }
-        
+
+      //  Initialize the result variable to store the maximum product  
       int result = 1;  
-      while( n > 4){
-          
-          result *= 3;
-          n -=3;
-      }
         
-      return result * n  ;
+        // Continue breaking down n into factors of 3 until n is greater than 4
+        
+        while( n > 4){
+            // Multiply the result by 3, effectively breaking down n into a factor of 3
+            result *= 3;
+            n -=3;
+        }
+        
+              
+        // At this point, n is either 2, 3, or 4. Multiply the remaining n with the result.
+        // This handles the remaining part of the integer break.  
+         return result * n  ;
         
     }
 }
