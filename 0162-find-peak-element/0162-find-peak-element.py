0a9200bcfc60c663c93d5da4df32c26655ce7255
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-02-29 17:25:31 +0530

    Time: 19 ms (94.36%), Space: 11.8 MB (83.86%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        return nums.index(max(nums)) 
