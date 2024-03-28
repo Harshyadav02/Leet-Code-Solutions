4d1fa0dddd372fc3b98535ff6890a32612a031c4
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-03-28 21:02:29 +0530

    Time: 43 ms (63.16%), Space: 87.2 MB (61.40%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -1,28 +1,32 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] arrayChange(int[] nums, int[][] operations) {
-
-        HashMap<Integer , Integer> numsHashMap = new HashMap<>();
-       
+        // Create a HashMap to store the indices of numbers in the original array
+        HashMap<Integer, Integer> numsHashMap = new HashMap<>();
         
-        
-        for(int index=0; index<nums.length; index++){
-
+        // Populate the HashMap with numbers as keys and their indices as values
+        for (int index = 0; index < nums.length; index++) {
             numsHashMap.put(nums[index], index);
         }
         
-        for(int array[] : operations){
-            
-            int index = numsHashMap.get(array[0]) ;
+        // Iterate through the operations array
+        for (int array[] : operations) {
+            // Extract the original and replacing values from the current operation
             int originalValue = array[0];
-            int replacingValue = array[1] ;
+            int replacingValue = array[1];
             
+            // Find the index of the original value in the original array using the HashMap
+            int index = numsHashMap.get(originalValue);
             
-            nums[index] = replacingValue ;
-            numsHashMap.put(replacingValue , numsHashMap.remove(originalValue)) ;
+            // Replace the value at the found index with the replacing value
+            nums[index] = replacingValue;
             
+            // Update the HashMap: remove the original value and add the replacing value with its index
+            numsHashMap.put(replacingValue, numsHashMap.remove(originalValue));
         }
         
-        return nums ;
-        
+        // Return the modified array
+        return nums;
     }
 }
