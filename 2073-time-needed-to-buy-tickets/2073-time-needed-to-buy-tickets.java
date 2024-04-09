bbde899183536080fe2a07cc85a91136a47ff3b6
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-04-09 11:27:38 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (32.97%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,18 +1,40 @@
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+//         int n = tickets.length;
+//         int timeTaken = 0;
+//         int[] remainingTickets = Arrays.copyOf(tickets, n);
+
+//         while (remainingTickets[k] > 0) {
+//             for (int i = 0; i < n && remainingTickets[k] > 0; i++) {
+//                 if (remainingTickets[i] > 0) {
+//                     timeTaken++;
+//                     remainingTickets[i]--; 
+//                 }
+//             }
+//         }
+
+//         return timeTaken;
+//     }
+// }
+
+/* Optimaized approch*/
+
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int n = tickets.length;
-        int timeTaken = 0;
-        int[] remainingTickets = Arrays.copyOf(tickets, n);
+        
+        int timeTaken = 0 ;
+        for(int index=0; index<tickets.length; index++){
 
-        while (remainingTickets[k] > 0) {
-            for (int i = 0; i < n && remainingTickets[k] > 0; i++) {
-                if (remainingTickets[i] > 0) {
-                    timeTaken++;
-                    remainingTickets[i]--; 
-                }
+            if(index<= k){
+                
+                timeTaken += Math.min(tickets[index], tickets[k]);
+            }
+            else{
+                timeTaken += Math.min(tickets[index], tickets[k]-1);
+            
             }
         }
-
-        return timeTaken;
+        return timeTaken ;
     }
+
 }
