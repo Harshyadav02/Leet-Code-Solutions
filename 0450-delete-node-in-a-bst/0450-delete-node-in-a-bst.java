ab2fbbb297a80b6f30087d74c3b2156e12f637cd
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-05-30 08:58:46 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (97.39%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,48 +14,6 @@
  * }
  */
 class Solution {
-    // public TreeNode deleteNode(TreeNode root, int key) {
-    //     // searching process
-    //     if(root==null) return root;
-    //     if(root.val < key){
-    //         root.right = deleteNode(root.right,key);
-    //     }
-    //     if(root.val>key){
-    //         root.left = deleteNode(root.left,key);
-    //     }
-
-    //     // if found
-    //     else{
-    //         // leaf Node
-    //         if(root.left==null && root.right==null){
-    //             return null;
-    //         }
-    //         // single child
-    //         if(root.right==null){
-    //             return root.left;
-    //         }
-    //         else if(root.left==null){
-    //             return root.right;
-    //         }
-
-    //         // two child
-    //         TreeNode IS = findInorderSuccessor(root.right);
-    //         root.val = IS.val;
-    //         root.right = deleteNode(root.right,IS.val);
-    //     }
-
-    //     return root;
-    // }
-
-    // private TreeNode findInorderSuccessor(TreeNode root){
-
-    //     while(root.left!=null){
-    //         root = root.left;
-    //     }
-    //     return root;
-    // }
-
-
 
     public TreeNode deleteNode(TreeNode root, int key) {
     // searching process
@@ -68,10 +26,13 @@ class Solution {
     } else {
         // if found
         // leaf Node or single child
-        if (root.left == null) {
-            return root.right;
-        } else if (root.right == null) {
+        if(root.left==null && root.right==null){
+            return null;
+        }
+        if (root.right == null) {
             return root.left;
+        } else if (root.left == null) {
+            return root.right;
         }
         
         // two children
