4f46179c2226d02aa10633ed0db89f29e92a8584
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-08-24 20:09:01 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (44.17%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int index = Arrays.binarySearch(nums,target);
+        if(index<0) return -1;
+        return index;
+    }
+}
