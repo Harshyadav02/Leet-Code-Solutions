1c167ab1e9b7cc22995575a3feb6296e3edadab6
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2024-09-02 08:54:08 +0530

    Time: 671 ms (21.17%), Space: 60.6 MB (6.23%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int chalkReplacer(int[] chalk, int k) {
+//         int index = 0;
+
+//         while(k>0 && chalk[index]<=k){
+//             if(index<chalk.length){
+//                 k -=chalk[index];
+//                 index++;
+
+//                 if(index==chalk.length){
+//                     index=0;
+//                 }
+//             }
+            
+//         }
+
+//         return index;
+//     }
+// }
+
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        int index = 0;
-
-        while(k>0 && chalk[index]<=k){
-            if(index<chalk.length){
-                k -=chalk[index];
-                index++;
-
-                if(index==chalk.length){
-                    index=0;
-                }
-            }
-            
+        
+        long sum = Arrays.stream(chalk).asLongStream().sum();
+        // System.out.println("sum\t"+sum);
+        while(sum<=k){
+            k -=sum;
         }
+        // System.out.println("k\t"+k);
+        if(k<=0) return 0;
+        for(int index=0;index<chalk.length; index++){
 
-        return index;
+            k -=chalk[index];
+            if(k<0) return index;
+        }
+        
+
+        return 0;
     }
 }
